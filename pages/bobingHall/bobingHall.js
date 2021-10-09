b4e499d6320ac3703c927ff42293f1bbf9191619
YY-nb <916295483@qq.com>
2021-10-09 21:17:17 +0800

    chybill的博饼大厅

--- pages/bobingHall/bobingHall.js
+++ pages/bobingHall/bobingHall.js
@@ -18,6 +18,11 @@ Component({
      * 组件的方法列表
      */
     methods: {
+        toPassword(){
+            wx.navigateTo({
+              url: '/pages/password/password',
+            })
+        }
 
     }
 })
